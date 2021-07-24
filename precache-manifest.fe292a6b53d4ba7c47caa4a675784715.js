687445447a0839ad0a245ca984f9ad685ec582fe
Lyucyukl <luckyshuklakunda@gmail.com>
2021-07-24 23:20:55 +0530

    Updates

---
+++ precache-manifest.fe292a6b53d4ba7c47caa4a675784715.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2437fb3a80020cfde2ab065dbc781e6d",
+    "url": "/grocery/index.html"
+  },
+  {
+    "revision": "15f2c1b97686f2386445",
+    "url": "/grocery/static/css/2.277ee59a.chunk.css"
+  },
+  {
+    "revision": "1281775ffa23d2441fd2",
+    "url": "/grocery/static/css/main.906be252.chunk.css"
+  },
+  {
+    "revision": "15f2c1b97686f2386445",
+    "url": "/grocery/static/js/2.18f2cc03.chunk.js"
+  },
+  {
+    "revision": "15254115b2ff4c50e4577bb514107508",
+    "url": "/grocery/static/js/2.18f2cc03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1281775ffa23d2441fd2",
+    "url": "/grocery/static/js/main.6be7e376.chunk.js"
+  },
+  {
+    "revision": "35e49a174ecf6c6ec45f",
+    "url": "/grocery/static/js/runtime-main.47d76574.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/grocery/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/grocery/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/grocery/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/grocery/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/grocery/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/grocery/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/grocery/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/grocery/static/media/slick.f97e3bbf.svg"
+  }
+]);
